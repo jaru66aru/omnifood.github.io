36d3ab37fd3c7bd744c1c64cf315c19cade9cf28
Jaruwat Arunpai <jaruwat.arunpai@gmail.com>
2021-04-11 21:28:30 +0700

    Commit new index

--- index.php
+++ index.php
@@ -325,14 +325,16 @@
             <div class="row">
                 <form method="POST" action="mailer.php" class="contact-form">
                     <div class="row">
-                        <?php
-                        <div class="form-messages success">
-                            Thank you! Your message has been sent.
-                        </div>
-                        <!--<div class="form-messages error">
-                            Oops! Something went wrong.
-                        </div> -->
-                        ?>
+                        
+                    <?php
+                    if($_GET['success'] == 1) {
+                        echo "<div class=\"form-messages success\">Thank you! Your message has been sent.</div>";
+                    }
+                    if($_GET['success'] == -1) {
+                        echo " <div class=\"form-messages error\">Oops! Something went wrong.</div>";
+                    }
+                    ?>
+
                     </div>
                     <div class="row">
                         <div class="col span-1-of-3">
